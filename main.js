e923539c7c7728b93d3fbb35e9d6de0d912d9e7e
Hrishikesh Menon <hrishikeshpmenon@gmail.com>
2019-12-05 22:04:39 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+< script >
+    let sliderImages = document.querySelectorAll('.slide'),
+        arrowLeft = document.querySelector('arrow-left'),
+        arrowRight = document.querySelector('arrow-right'),
+        current = 0;
+
+function reset() {
+    for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = sliderImage = 'none';
+    }
+}
+
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = 'block';
+}
+
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = 'block';
+    current--;
+}
+arrowLeft.addEventListener('click', function() {
+    if (current === 0) {
+        current = sliderImages.length;
+    }
+    slideLeft();
+
+});
+
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = 'block';
+    current++;
+}
+arrowRight.addEventListener('click', function() {
+    if (current === sliderImages.length - 1) {
+        current = -1;
+    }
+    slideRight();
+
+});
+
+startSlide(); <
+/script>
